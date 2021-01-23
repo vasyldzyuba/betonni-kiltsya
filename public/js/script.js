c678b8d76f53e15225b9986e128981f436844aad
Vasyl Dziuba <vasyl.dzuba1@gmail.com>
2021-01-23 16:13:10 +0200

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+
+
+  $(window).bind("scroll", function () {
+    if ($(this).scrollTop() > 50) {
+      $(".arrow-up").fadeIn(400);
+    } else {
+      $('.arrow-up').fadeOut(400);
+    }
+  });
+  // window.onload = function () {
+  //   myFunction();
+  // }
+  setTimeout(function(){ 
+    $('.load').css({
+      display: "none"
+    }); 
+  }, 1300);
+    // function myFunction() {
+    
+    // }
+  $(".ham").on("click", function () {
+    $(".main-ul-none").css({
+      display: "block",
+    });
+    $(".close-btn-ham").css({
+      display: "block",
+    });
+    $(".arrow-up").css({
+      display: "none",
+    });
+    $("html").css({
+      overflow: "hidden"
+    });
+  });
+  $(".close-btn-ham").on("click", function () {
+    $("html").css({
+      overflow: "scroll",
+    });
+    $(".main-ul-none").css({
+      display: "none",
+    });
+    $(this).css({
+      display: "none",
+    });
+    $(".arrow-up").css({
+      display: "block",
+    });
+  });
+  $(".arrow-up").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: "0px",
+      },
+      100
+    );
+  });
+  $(".clicked-link").on("click", function () {
+    $("html").css({
+      overflow: "scroll",
+    });
+    $(".main-ul-none").css({
+      display: "none",
+    });
+    $(".close-btn-ham").css({
+      display: "none",
+    });
+    $(".arrow-up").css({
+      display: "block",
+    });
+  });
+});
